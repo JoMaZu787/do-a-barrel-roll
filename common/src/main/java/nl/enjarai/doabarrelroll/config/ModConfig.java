54ea90146cf826d419c3057f1a2129c03d332884
JoMaZu787 <63924520+JoMaZu787@users.noreply.github.com>
2024-07-08 22:45:29 +0200

    Update ModConfig.java

--- common/src/main/java/nl/enjarai/doabarrelroll/config/ModConfig.java
+++ common/src/main/java/nl/enjarai/doabarrelroll/config/ModConfig.java
@@ -171,19 +171,7 @@ public class ModConfig {
     }
 
     public boolean getEnableThrust() {
-        if (general.thrust.enable_thrust) {
-            ClientPlayerEntity player;
-            if (DoABarrelRollClient.isConnectedToRealms() &&
-                    (player = MinecraftClient.getInstance().player) != null && player.hasPermissionLevel(2)) {
-                return true;
-            }
-
-            return DoABarrelRollClient.HANDSHAKE_CLIENT.getConfig()
-                    .map(LimitedModConfigServer::allowThrusting)
-                    .orElse(false);
-        }
-
-        return false;
+        return general.thrust.enable_thrust;
     }
 
     public boolean getEnableThrustClient() {
